1d5bf7f2e64410d58596fb78962357c86023d3e9
David Brownell <db@DavidBrownell.com>
2024-02-09 14:09:32 -0500

    📝 Switched to MIT license

--- .github/tools/CreatePythonBinaryArchive.py
+++ .github/tools/CreatePythonBinaryArchive.py
@@ -8,9 +8,7 @@
 # ----------------------------------------------------------------------
 # |
 # |  Copyright David Brownell 2024
-# |  Distributed under the Boost Software License, Version 1.0. See
-# |  accompanying file LICENSE_1_0.txt or copy at
-# |  http://www.boost.org/LICENSE_1_0.txt.
+# |  Distributed under the MIT License.
 # |
 # ----------------------------------------------------------------------
 """Searches for a python binary output directory and then creates an archive from it."""
