7f6f7e280f2660ee2a3043e7ff51dab97166414b
David Brownell <db@davidbrownell.com>
2024-02-16 09:54:06 -0500

    ✨ [+feature] Functionality to create dev environment docker images
    Includes code and tests and GitHub Action workflows to invoke the functionality as part of CI builds.

--- src/dbrownell_DevTools/BuildActivities.py
+++ src/dbrownell_DevTools/BuildActivities.py
@@ -13,12 +13,22 @@
 # ----------------------------------------------------------------------
 """Functionality often invoked as part of a build process"""
 
+import os
+import textwrap
+import uuid
+
+from contextlib import contextmanager
+from datetime import datetime
 from pathlib import Path
-from typing import Callable, Optional
+from typing import Callable, Generator, Optional, Protocol
 
 from AutoGitSemVer.Lib import GetSemanticVersion  # type: ignore [import-untyped]
+from dbrownell_Common.ContextlibEx import ExitStack  # type: ignore [import-untyped]
+from dbrownell_Common import PathEx  # type: ignore [import-untyped]
 from dbrownell_Common.Streams.DoneManager import DoneManager  # type: ignore[import-untyped]
+from dbrownell_Common import SubprocessEx  # type: ignore [import-untyped]
 from semantic_version import Version as SemVer  # type: ignore [import-untyped]
+import git
 
 
 # ----------------------------------------------------------------------
@@ -48,3 +58,329 @@ def UpdateVersion(
 
         with version_filename.open("w") as f:
             f.write(content)
+
+
+# ----------------------------------------------------------------------
+class CreateBaseImageFunc(Protocol):
+    """Callback passed to CreateDockerImage."""
+
+    def __call__(
+        self,
+        dm: DoneManager,
+    ) -> Generator[str, None, None]:
+        """\
+        Creates a base image to use as the foundation for the docker image.
+
+        Returns (image_name, True if the docker image should be deleted)
+        """
+        ...  # pragma: no cover
+
+
+def CreateDockerImage(
+    dm: DoneManager,
+    repo_root: Path,
+    create_base_image_func: Optional[CreateBaseImageFunc] = None,
+    bootstrap_args: str = "--package",
+    name_suffix: Optional[str] = None,  # Decorates the docker image name and output file
+) -> None:
+    create_base_image_func = create_base_image_func or _CreateDefaultBaseImage  # type: ignore
+    name_suffix = name_suffix or ""
+
+    # Extract the repo name
+    repo_name: Optional[str] = None
+
+    with dm.Nested(
+        "Extracting repository info...",
+        lambda: repo_name or "<Error>",
+    ):
+        repo = git.Repo(repo_root)
+
+        repo_name = repo.remotes.origin.url.split(".git")[0].split("/")[-1]
+
+    # Create the base image
+    with create_base_image_func(dm) as base_image_name:  # type: ignore
+        if dm.result != 0:
+            return
+
+        with _CreateDockerContainer(dm, repo_root, bootstrap_args, base_image_name) as container_id:
+            if dm.result != 0:
+                return
+
+            with _SaveTemporaryImage(dm, container_id) as temporary_image_id:
+                if dm.result != 0:
+                    return
+
+                with _CreateFinalImage(
+                    dm,
+                    name_suffix,
+                    repo_name,
+                    temporary_image_id,
+                ) as final_image_id:
+                    if dm.result != 0:
+                        return
+
+                    _ExtractImage(
+                        dm,
+                        repo_root,
+                        name_suffix,
+                        final_image_id,
+                    )
+
+
+# ----------------------------------------------------------------------
+def StreamCommand(
+    dm: DoneManager,
+    header: str,
+    command_line: str,
+    *suffix_funcs,
+    cwd: Optional[Path] = None,
+) -> None:
+    with dm.Nested(
+        header,
+        list(suffix_funcs),
+        suffix="\n",
+    ) as this_dm:
+        this_dm.WriteVerbose("Command Line: {}\n\n".format(command_line))
+
+        with this_dm.YieldStream() as stream:
+            this_dm.result = SubprocessEx.Stream(
+                command_line,
+                stream,
+                cwd=cwd,
+            )
+
+
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+# ----------------------------------------------------------------------
+@contextmanager
+def _CreateDefaultBaseImage(
+    dm: DoneManager,
+) -> Generator[str, None, None]:
+    unique_id = str(uuid.uuid4()).replace("-", "")
+
+    docker_filename = Path(f"{unique_id}.dockerfile")
+
+    with dm.Nested("Creating base image Dockerfile..."):
+        with docker_filename.open("w") as f:
+            f.write(
+                textwrap.dedent(
+                    """\
+                    FROM ubuntu:latest
+
+                    RUN apt-get update \
+                     && apt-get install -y \
+                            build-essential \
+                            curl \
+                            git \
+                     && apt-get clean \
+                     && rm -rf /var/lib/apt/lists/*
+
+                    SHELL ["/bin/bash", "-c"]
+                    """,
+                ),
+            )
+
+    with ExitStack(docker_filename.unlink):
+        StreamCommand(
+            dm,
+            "Building base image...",
+            f"docker build --tag {unique_id} --file {docker_filename.name} .",
+        )
+
+        if dm.result != 0:
+            yield ""
+            return
+
+        with ExitStack(
+            lambda: StreamCommand(
+                dm,
+                "Deleting base image...",
+                f"docker image rm {unique_id}",
+            ),
+        ):
+            yield unique_id
+
+
+# ----------------------------------------------------------------------
+@contextmanager
+def _CreateDockerContainer(
+    dm: DoneManager,
+    repo_root: Path,
+    bootstrap_args: str,
+    base_image_name: str,
+) -> Generator[str, None, None]:
+    unique_id = str(uuid.uuid4()).replace("-", "")
+
+    script_filename = repo_root / f"{unique_id}.sh"
+
+    with dm.Nested("Creating initialization script..."):
+        with script_filename.open("w", newline="\n") as f:
+            f.write(
+                textwrap.dedent(
+                    f"""\
+                    #!/bin/bash
+
+                    git clone /local code
+
+                    cd code
+                    ./Bootstrap.sh {bootstrap_args}
+
+                    echo "source /code/Activate.sh" >> /etc/bash.bashrc
+
+                    . ./Activate.sh
+                    pip cache purge
+                    micromamba clean --all --force-pkgs-dirs --quiet --trash --yes
+                    """,
+                ),
+            )
+
+        script_filename.chmod(0o755)
+
+    with ExitStack(script_filename.unlink):
+        # Run docker and invoke the initialization script
+        StreamCommand(
+            dm,
+            "Creating the temporary container...",
+            f'docker run --name {unique_id} --volume "{repo_root}:/local" {base_image_name} /local/{script_filename.name}',
+        )
+
+        if dm.result != 0:
+            yield ""
+            return
+
+    with ExitStack(
+        lambda: StreamCommand(
+            dm,
+            "Deleting the temporary container...",
+            f"docker container rm {unique_id}",
+        ),
+    ):
+        yield unique_id
+
+
+# ----------------------------------------------------------------------
+@contextmanager
+def _SaveTemporaryImage(
+    dm: DoneManager,
+    container_id: str,
+) -> Generator[str, None, None]:
+    unique_id = str(uuid.uuid4()).replace("-", "")
+
+    StreamCommand(
+        dm,
+        "Saving temporary image...",
+        f"docker commit {container_id} {unique_id}",
+    )
+
+    if dm.result != 0:
+        yield ""
+        return
+
+    with ExitStack(
+        lambda: StreamCommand(
+            dm,
+            "Deleting temporary image...",
+            f"docker image rm {unique_id}",
+        ),
+    ):
+        yield unique_id
+
+
+# ----------------------------------------------------------------------
+@contextmanager
+def _CreateFinalImage(
+    dm: DoneManager,
+    name_suffix: str,
+    repo_name: str,
+    temporary_image_id: str,
+) -> Generator[str, None, None]:
+    now = datetime.now()
+    image_name = f"{repo_name}{name_suffix}-{now.year:04d}.{now.month:02d}.{now.day:02d}-{now.hour:02d}.{now.minute:02d}.{now.second:02d}".lower()
+
+    docker_filename = Path(f"{image_name}.dockerfile")
+
+    with dm.Nested("Creating final Dockerfile..."):
+        with docker_filename.open("w") as f:
+            f.write(
+                textwrap.dedent(
+                    f"""\
+                    FROM {temporary_image_id}
+
+                    WORKDIR /code
+
+                    ENTRYPOINT ["/bin/bash", "--login"]
+                    """,
+                ),
+            )
+
+    with ExitStack(docker_filename.unlink):
+        StreamCommand(
+            dm,
+            "Building final image...",
+            f"docker build --tag {image_name} --file {docker_filename.name} .",
+        )
+
+        if dm.result != 0:
+            yield ""
+            return
+
+        with ExitStack(
+            lambda: StreamCommand(
+                dm,
+                "Deleting final image...",
+                f"docker image rm {image_name}",
+            ),
+        ):
+            yield image_name
+
+
+# ----------------------------------------------------------------------
+def _ExtractImage(
+    dm: DoneManager,
+    repo_root: Path,
+    name_suffix: str,
+    final_image_id: str,
+) -> None:
+    if os.name == "nt":
+        compressed_filename = repo_root / f"docker_image{name_suffix}.tar"
+
+        # ----------------------------------------------------------------------
+        def Postprocess():
+            with ExitStack(compressed_filename.unlink):
+                # Zip the tarball
+                final_filename = compressed_filename.with_suffix(".tar.zip")
+
+                if final_filename.is_file():
+                    with dm.Nested("Removing previous compressed docker image..."):
+                        final_filename.unlink()
+
+                StreamCommand(
+                    dm,
+                    "Compressing docker image...",
+                    f'PowerShell -Command "Compress-Archive -Path {compressed_filename} -DestinationPath {final_filename}"',
+                    lambda: PathEx.GetSizeDisplay(final_filename),
+                )
+
+        # ----------------------------------------------------------------------
+
+        postprocess_func = Postprocess
+    else:
+        compressed_filename = repo_root / f"docker_image{name_suffix}.tar.gz"
+        postprocess_func = lambda *args, **kwargs: None
+
+    if compressed_filename.is_file():
+        with dm.Nested("Removing previous docker image..."):
+            compressed_filename.unlink()
+
+    StreamCommand(
+        dm,
+        "Saving docker image...",
+        f'docker save --output "{compressed_filename}" {final_image_id}',
+        lambda: PathEx.GetSizeDisplay(compressed_filename),
+    )
+
+    if dm.result != 0:
+        return
+
+    postprocess_func()
---
+++ tests/BuildActivities_EndToEndTest.py
@@ -0,0 +1,180 @@
+# ----------------------------------------------------------------------
+# |
+# |  BuildActivities_EndToEndTest.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2024-02-16 07:27:38
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2024
+# |  Distributed under the MIT License.
+# |
+# ----------------------------------------------------------------------
+"""End-to-end tests for BuildActivities.py"""
+
+import os
+import re
+import sys
+import uuid
+
+from pathlib import Path
+from typing import Optional
+
+from dbrownell_Common.ContextlibEx import ExitStack
+from dbrownell_Common import PathEx
+from dbrownell_Common import SubprocessEx
+from dbrownell_Common.Streams.DoneManager import DoneManager
+from dbrownell_Common.TestHelpers.StreamTestHelpers import GenerateDoneManagerAndContent
+import pytest
+
+from dbrownell_DevTools.BuildActivities import CreateDockerImage, StreamCommand
+
+
+# ----------------------------------------------------------------------
+def SkipCreateDockerImageDecorator():
+    should_skip = False
+    reason = ""
+
+    if SubprocessEx.Run("docker --version").returncode != 0:
+        should_skip = True
+        reason = "Docker is not running"
+    elif os.name == "nt":
+        result = SubprocessEx.Run("docker info")
+        assert result.returncode == 0, result.output
+
+        if "OSType: windows" in result.output:
+            should_skip = True
+            reason = "Docker is configured to build windows containers"
+
+    return pytest.mark.skipif(should_skip, reason=reason)
+
+
+# ----------------------------------------------------------------------
+@SkipCreateDockerImageDecorator()
+def test_CreateDockerImage():
+    unique_id = "-{}".format(str(uuid.uuid4()).replace("-", ""))
+    repo_root = Path(__file__).parent.parent
+
+    sys.stdout.write("\n")
+    with DoneManager.Create(sys.stdout, "Testing") as dm:
+        with dm.Nested("Creating docker image...") as creating_dm:
+            CreateDockerImage(
+                creating_dm,
+                repo_root,
+                bootstrap_args="--package --verbose",
+                name_suffix=unique_id,
+            )
+
+            assert creating_dm.result == 0
+
+        with dm.Nested("Testing docker image...") as test_dm:
+            if os.name == "nt":
+                compressed_filename = PathEx.EnsureFile(
+                    repo_root / f"docker_image{unique_id}.tar.zip"
+                )
+
+                with ExitStack(compressed_filename.unlink):
+                    # Decompress the file
+                    StreamCommand(
+                        test_dm,
+                        "Decompressing...",
+                        f"""PowerShell -Command "Expand-Archive -Path '{compressed_filename}' -DestinationPath .""",
+                        cwd=repo_root,
+                    )
+
+                    assert test_dm.result == 0
+
+                image_filename = PathEx.EnsureFile(repo_root / f"docker_image{unique_id}.tar")
+            else:
+                image_filename = PathEx.EnsureFile(repo_root / f"docker_image{unique_id}.tar.gz")
+
+            with ExitStack(image_filename.unlink):
+                image_name: Optional[str] = None
+
+                with test_dm.Nested("Loading image...") as load_dm:
+                    result = SubprocessEx.Run(f'docker load --input "{image_filename}"')
+                    assert result.returncode == 0
+
+                    match = re.match(r"Loaded image: (?P<name>.+)\r?\n", result.output)
+                    assert match, result.output
+
+                    image_name = match.group("name")
+
+                assert image_name is not None
+
+            with ExitStack(
+                lambda: StreamCommand(
+                    test_dm,
+                    "Deleting image...",
+                    f'docker image rm "{image_name}"',
+                ),
+            ):
+                with test_dm.Nested("Running container...") as run_dm:
+                    with run_dm.Nested("Starting container..."):
+                        # Start the container
+                        result = SubprocessEx.Run(
+                            f"docker run -itd --volume {repo_root}:/local {image_name}"
+                        )
+                        assert result.returncode == 0, result.output
+
+                        container_id = result.output.strip()
+
+                    # ----------------------------------------------------------------------
+                    def StopAndDelete():
+                        StreamCommand(
+                            run_dm,
+                            "Stopping container...",
+                            f"docker stop {container_id}",
+                        )
+                        assert run_dm.result == 0
+
+                        StreamCommand(
+                            run_dm,
+                            "Deleting container...",
+                            f"docker container rm {container_id}",
+                        )
+                        assert run_dm.result == 0
+
+                    # ----------------------------------------------------------------------
+
+                    with ExitStack(StopAndDelete):
+                        with run_dm.Nested("Testing container..."):
+                            # Note that it isn't possible to automate the execution of scripts within
+                            # the container, as it is configured to activate the local environment
+                            # when logging in isn't considered to be an interactive shell when running
+                            # a container with a command. And, it doesn't seem possible to exec python
+                            # when operating on a started container. So, instead of running the
+                            # functionality, we use the presence of files to determine if the image has
+                            # been built correctly.
+                            #
+                            # There is almost certainly a better way to do this.
+
+                            # Check for the presence of files
+                            result = SubprocessEx.Run(f"docker exec {container_id} ls -al")
+                            assert result.returncode == 0, result.output
+                            assert "Activate.sh" in result.output
+                            assert "Deactivate.sh" in result.output
+
+                            # Copy Build.py locally
+                            copied_build_filename = f"Build.py-{container_id}"
+
+                            result = SubprocessEx.Run(
+                                f"docker exec {container_id} cp Build.py /local/{copied_build_filename}"
+                            )
+                            assert result.returncode == 0, result.output
+
+                            copied_build_filename = PathEx.EnsureFile(
+                                repo_root / copied_build_filename
+                            )
+
+                            with ExitStack(copied_build_filename.unlink):
+                                original_build_filename = PathEx.EnsureFile(repo_root / "Build.py")
+
+                                with copied_build_filename.open() as f:
+                                    copied_build_content = f.read()
+
+                                with original_build_filename.open() as f:
+                                    original_build_content = f.read()
+
+                                assert copied_build_content == original_build_content
